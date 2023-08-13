7c833febe78bd851975cb1eeecc2828b75fbd44c
Giovane Santos <giovane98sp@gmail.com>
2023-08-13 12:06:51 -0300

    add getPosts in home page

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,4 +1,42 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+import './Home.css';
+import { Link } from 'react-router-dom';
 
 export const Home = () => {
-  return <h1>Home</h1>
-}
+  const [posts, setPosts] = useState([]);
+
+  const getPosts = async () => {
+    try {
+      const res = await axios.get('https://jsonplaceholder.typicode.com/posts');
+      const data = res.data;
+      setPosts(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getPosts();
+  }, []);
+
+  return (
+    <div>
+      <h1>Últimos posts</h1>
+      {posts.length === 0 ? (
+        <p>Carregando...</p>
+      ) : (
+        posts.map((post) => (
+          <div className="post" key={post.id}>
+            <h2>{post.title}</h2>
+            <p>{post.body}</p>
+            <Link to={`/posts/${post.id}`} className="btn">
+              Ler mais
+            </Link>
+          </div>
+        ))
+      )}
+    </div>
+  );
+};
